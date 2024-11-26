ac862c6653e59e821d07357b92f060505598f7e8
Sengeki1 <gamersengeki@gmail.com>
2024-11-26 13:55:54 -0100

    Init Configuration's

---
+++ default.frag
@@ -0,0 +1,7 @@
+#version 400
+
+void main() {
+
+	gl_FragColor = vec4(gl_FragCoord.xy, 0.0, 1.0);
+
+}
--- main.cpp
+++ main.cpp
@@ -2,6 +2,40 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include "shaderClass.h"
+
 int main() {
+
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4); // Major version of OpenGl we are using which is 3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // to ensure which package we use (Core - contain modern functions
+
+	GLFWwindow* window = glfwCreateWindow(600, 600, "Window", NULL, NULL);
+
+	if (window == NULL) {
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+	gladLoadGL();
+	glViewport(0, 0, 600, 600);
+
+	Shader shader("default.vert", "default.frag");
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		shader.Activate();
+
+		glfwSwapBuffers(window);
+	}
+
+
 	return 0;
 }
